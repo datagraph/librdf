bd11a01ae598b2edb106a8b02d3d73fd8cf49bce
Arto Bendiken <arto@bendiken.net>
2013-03-06 19:07:20 +0100

    Added an "extern.h" C header for each of the subproject libraries.

---
+++ extern.h
@@ -0,0 +1,14 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef RDFXX_EXTERN_H
+#define RDFXX_EXTERN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* RDFXX_EXTERN_H */
