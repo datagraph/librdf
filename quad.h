bb683fc2f363c6672d86cff53e1d2a848fd40e74
Arto Bendiken <arto@bendiken.net>
2013-03-10 19:10:28 +0100

    Defined the rdf::triple and rdf::quad classes.

--- quad.h
+++ quad.h
@@ -4,6 +4,26 @@
 #define RDFXX_QUAD_H
 
 namespace rdf {
+  struct term;
+
+  struct quad {
+    public:
+      term* subject   = nullptr;
+      term* predicate = nullptr;
+      term* object    = nullptr;
+      term* context   = nullptr;
+
+      quad(term* subject, term* predicate, term* object)
+        : subject(subject),
+          predicate(predicate),
+          object(object) {}
+
+      quad(term* subject, term* predicate, term* object, term* context)
+        : subject(subject),
+          predicate(predicate),
+          object(object),
+          context(context) {}
+  };
 }
 
 #endif /* RDFXX_QUAD_H */
--- triple.h
+++ triple.h
@@ -4,6 +4,19 @@
 #define RDFXX_TRIPLE_H
 
 namespace rdf {
+  struct term;
+
+  struct triple {
+    public:
+      term* subject   = nullptr;
+      term* predicate = nullptr;
+      term* object    = nullptr;
+
+      triple(term* subject, term* predicate, term* object)
+        : subject(subject),
+          predicate(predicate),
+          object(object) {}
+  };
 }
 
 #endif /* RDFXX_TRIPLE_H */
