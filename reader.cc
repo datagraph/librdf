2240c22cc9b32132ad636c39671625bda1e6c636
Arto Bendiken <arto@bendiken.net>
2013-03-10 17:55:46 +0100

    Implemented the rdf::reader class constructor and destructor.

--- reader.cc
+++ reader.cc
@@ -5,3 +5,90 @@
 #endif
 
 #include "rdf++/reader.h"
+
+#include <cassert>   /* for assert() */
+#include <new>       /* for std::bad_alloc */
+#include <stdexcept> /* for std::invalid_argument, std::runtime_error */
+
+#include <raptor2/raptor2.h> /* for raptor_*() */
+
+using namespace rdf;
+
+class reader::implementation : private boost::noncopyable {
+  public:
+    implementation(
+      const std::string& content_type,
+      const std::string& charset,
+      const std::string& base_uri);
+
+    ~implementation();
+
+  private:
+    const std::string _content_type;
+    const std::string _charset;
+    raptor_world* _world = nullptr;
+    raptor_uri* _base_uri = nullptr;
+    raptor_parser* _parser = nullptr;
+    raptor_statement* _statement = nullptr;
+};
+
+reader::implementation::implementation(const std::string& content_type,
+                                       const std::string& charset,
+                                       const std::string& base_uri)
+  : _content_type(content_type),
+    _charset(charset) {
+
+  _world = raptor_new_world();
+  if (_world == nullptr) {
+    throw std::bad_alloc(); /* out of memory */
+  }
+
+  _base_uri = raptor_new_uri(_world, (const unsigned char*)base_uri.c_str());
+  if (_base_uri == nullptr) {
+    raptor_free_world(_world), _world = nullptr;
+    throw std::bad_alloc(); /* out of memory */
+  }
+
+  std::string parser_name("nquads"); // TODO
+  assert(!parser_name.empty());
+
+  _parser = raptor_new_parser(_world, parser_name.c_str());
+  if (_parser == nullptr) {
+    raptor_free_uri(_base_uri), _base_uri = nullptr;
+    raptor_free_world(_world), _world = nullptr;
+    throw std::bad_alloc(); /* out of memory */
+  }
+
+  _statement = raptor_new_statement(_world);
+  if (_statement == nullptr) {
+    raptor_free_parser(_parser), _parser = nullptr;
+    raptor_free_uri(_base_uri), _base_uri = nullptr;
+    raptor_free_world(_world), _world = nullptr;
+    throw std::bad_alloc(); /* out of memory */
+  }
+}
+
+reader::implementation::~implementation() {
+  if (_statement != nullptr) {
+    raptor_free_statement(_statement), _statement = nullptr;
+  }
+
+  if (_parser != nullptr) {
+    raptor_free_parser(_parser), _parser = nullptr;
+  }
+
+  if (_base_uri != nullptr) {
+    raptor_free_uri(_base_uri), _base_uri = nullptr;
+  }
+
+  if (_world != nullptr) {
+    raptor_free_world(_world), _world = nullptr;
+  }
+}
+
+reader::reader(const std::string& content_type,
+               const std::string& charset,
+               const std::string& base_uri)
+  : _implementation(new reader::implementation(content_type, charset, base_uri)) {}
+
+reader::~reader() = default;
--- reader.h
+++ reader.h
@@ -3,7 +3,26 @@
 #ifndef RDFXX_READER_H
 #define RDFXX_READER_H
 
+#include <memory> /* for std::unique_ptr */
+#include <string> /* for std::string */
+
+#include <boost/noncopyable.hpp>
+
 namespace rdf {
+  class reader : private boost::noncopyable {
+    public:
+      reader(
+        const std::string& content_type,
+        const std::string& charset,
+        const std::string& base_uri);
+
+      ~reader();
+
+    private:
+      class implementation;
+
+      std::unique_ptr<implementation> _implementation;
+  };
 }
 
 #endif /* RDFXX_READER_H */
