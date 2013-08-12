f2266c1cd26271a2a0526c84d04e7f01d2c909b4
Arto Bendiken <arto@bendiken.net>
2013-08-12 17:06:08 +0200

    Switched over to using Clang as the default development compiler.

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #/bin/sh
-export CC='ccache gcc -std=c11'
-export CXX='ccache g++ -std=c++11'
-export CFLAGS='-g -Os'
-export CXXFLAGS='-g -Os'
+export CC='ccache clang -std=c11 -Qunused-arguments -fcolor-diagnostics'
+export CXX='ccache clang++ -std=c++11 -Qunused-arguments -fcolor-diagnostics'
 export CPPFLAGS=''
+export CFLAGS='-g -Os'
+export CXXFLAGS="$CFLAGS"
 export LDFLAGS=''
 ./configure $*
