40f7fcea46c830d11442505f77249b323a1c5fc5
Arto Bendiken <arto@bendiken.net>
2013-09-27 15:00:21 +0200

    Implemented some basic unit tests for the TriX reader.

--- test/rdf++/check_reader_trix.cc
+++ test/rdf++/check_reader_trix.cc
@@ -4,6 +4,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <rdf++/reader.h> /* for rdf::reader */
+#include <rdf++/writer.h> /* for rdf::writer */
 
 #include <rdf++/quad.h>   /* for rdf::quad */
 #include <rdf++/term.h>   /* for rdf::term */
@@ -11,6 +12,33 @@
 
 #include <cstdio>         /* for stdout */
 
+static FILE*
+examples() {
+  return fopen("examples.xml", "r");
+}
+
 BOOST_AUTO_TEST_CASE(test_ctor) {
+  BOOST_CHECK(true); // TODO
+}
+
+BOOST_AUTO_TEST_CASE(test_read_triples) {
+  rdf::writer writer(stdout, "application/trix", "UTF-8", "http://example.org/");
+  rdf::reader reader(examples(), "application/trix", "UTF-8", "http://example.org/");
+  writer.begin();
+  reader.read_triples([&](std::unique_ptr<rdf::triple> triple) {
+    writer.write_triple(*triple);
+  });
+  writer.finish();
+  BOOST_CHECK(true); // TODO
+}
+
+BOOST_AUTO_TEST_CASE(test_read_quads) {
+  rdf::writer writer(stdout, "application/trix", "UTF-8", "http://example.org/");
+  rdf::reader reader(examples(), "application/trix", "UTF-8", "http://example.org/");
+  writer.begin();
+  reader.read_quads([&](std::unique_ptr<rdf::quad> quad) {
+    writer.write_quad(*quad);
+  });
+  writer.finish();
   BOOST_CHECK(true); // TODO
 }
