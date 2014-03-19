73f458e436d6ed2beb39e75b87e098872dd51275
Arto Bendiken <arto@bendiken.net>
2014-03-19 18:25:23 +0100

    Ensured that the test suite source files include the <config.h> header.

--- test/check_writer_trig.cc
+++ test/check_writer_trig.cc
@@ -1,5 +1,9 @@
 /* This is free and unencumbered software released into the public domain. */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "catch.hpp"
 
 #include <rdf++/writer.h> /* for rdf::writer */
